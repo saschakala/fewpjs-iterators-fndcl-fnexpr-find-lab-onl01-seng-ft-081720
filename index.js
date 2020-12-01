993cb28f704ac697055b056396ac4d1673e744c2
“saschakala” <mxsaschakala@gmail.com>
2020-12-01 12:50:54 -0500

    commit tree 1fe9f83d36e5cd6cf41e49daaaf2c0a7181b6dcd

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+  let win = record.find(object => object["result"] === "W"
+  )
+
+  if (win) {
+    return win["year"];
+  } else {
+    return undefined;
+  }
+}
+
